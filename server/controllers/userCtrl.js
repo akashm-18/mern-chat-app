df26a97aac3650fd7a1e2481e2c02955c62a5a8b
akashm-18 <akashmurugan338@gmail.com>
2024-04-09 19:24:48 +0530

    checking repo

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -5,9 +5,11 @@ const getUsersForSidebar = async (req , res) => {
         
         const loggedInUser = req.user._id
         const filteredUsers = await User.find({ _id : { $ne : loggedInUser } }).select("-password")
+        console.log("checking the github repo")
         res.status(200).json(filteredUsers)
 
     } catch (error) {
+
         console.log("Error in the getusers controller" , error.message)
         res.status(500).json({ error : error.message || "Internal server error" })
     }
