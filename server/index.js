2f61eb1520d04a367f037e89a2a2bff4ff37e87b
akashm-18 <akashmurugan338@gmail.com>
2024-04-18 00:14:05 +0530

    Ready to deploy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import cors from "cors"
+
+import path from 'path'
 
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
@@ -14,6 +15,8 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 // app.use(cors({
@@ -23,6 +26,12 @@ app.use(cookieParser())
 app.use('/api/auth' , authRoutes)
 app.use('/api/messages' , messageRoutes)
 app.use('/api/users' , userRoutes)
+
+app.use(express.static(path.join(__dirname , "/client/dist")))
+
+app.get("*" , (req,res) => {
+    res.sendFile(path.join(__dirname , "client" , "dist" , "index.html"))
+})
 
 server.listen(PORT , () => {
     connectToMongoDb()
